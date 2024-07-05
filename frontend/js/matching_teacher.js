9367eb858ddc7281db2a17bf5eceedcfdae15211
yeeeww <yeangsshi@ewhain.net>
2024-07-06 03:02:50 +0900

    티칭계획

--- frontend/js/matching_teacher.js
+++ frontend/js/matching_teacher.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const skills = document.querySelectorAll('.skill .checkbox');
+mdocument.addEventListener("DOMContentLoaded", function () {
+  const skills = document.querySelectorAll(".skill .checkbox");
 
-    skills.forEach(skill => {
-        skill.addEventListener('change', () => {
-            const label = skill.nextElementSibling;
-            if (skill.checked) {
-                label.classList.add('active');
-            } else {
-                label.classList.remove('active');
-            }
-        });
+  skills.forEach((skill) => {
+    skill.addEventListener("change", () => {
+      const label = skill.nextElementSibling;
+      if (skill.checked) {
+        label.classList.add("active");
+      } else {
+        label.classList.remove("active");
+      }
     });
+  });
 });
